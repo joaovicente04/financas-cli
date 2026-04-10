48bda39b43fb0bce509d77686eb3dc17add1d0ba
João Vicente Burin Souza <joao.souza@cna.org.br>
2026-04-10 08:12:45 -0300

    corrige descoberta de testes no CI

--- tests/test_finance.py
+++ tests/test_finance.py
@@ -1,13 +1,6 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from src.finance import add_expense, total_expenses
-
-def test_add_expense():
-    expenses = []
-    expenses.append(add_expense("lanche", 10))
-    assert len(expenses) == 1
-
-def test_total():
-    expenses = [
-        {"description": "a", "amount": 10},
-        {"description": "b", "amount": 20}
-    ]
-    assert total_expenses(expenses) == 30
